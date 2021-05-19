19d2b69250b4c34001b8912da905bf57797bd71a
smanwarulislam <engg.anwarbd@gmail.com>
2021-05-19 23:01:43 +0600

    Add exercise: number, string, boolean, comparisons

---
+++ 11_JavaScript/exercise_number_string_boolean_comparisons/exercise1_SOLUTIONS.js
@@ -0,0 +1,85 @@
+// Guess what answers you would get if you run this in the Javascript Console in Google Chrome.
+
+// Once you have an answer to the questions then check them by copying them and running them in the console yourself line by line.
+
+// Evaluate the below:
+
+// Chrome DevTools Console (See the Chrome DevTools Console)
+
+5 + "34";
+// expected output: "534"
+
+5 - "4";
+// expected output: 1
+
+10 % 5;
+// expected output: 0
+
+5 % 10;
+// expected output: 5
+
+"Java" + "Script";
+// expected output: "JavaScript"
+
+" " + " ";
+// expected output: "  "
+
+" " + 0;
+// expected output: " 0"
+
+true + true;
+// expected output: 2
+
+true + false;
+// expected output: 1
+
+false + true;
+// expected output: 1
+
+false - true;
+// expected output: -1
+
+3 - 4;
+// expected output: -1
+
+"Bob" - "bill";
+// expected output: NaN
+
+// Evaluate the below comparisons:
+
+5 >= 1;
+// expected output: true
+
+0 === 1;
+// expected output: false
+
+4 <= 1;
+// expected output: false
+
+1 != 1;
+// expected output: false
+
+"A" > "B";
+// expected output: false
+
+"B" < "C";
+// expected output: true
+
+"a" > "A";
+// expected output: true
+
+"b" < "A";
+// expected output: false
+
+true === false;
+// expected output: false
+
+true != true;
+// expected output: false
+
+// Make the string:
+
+"Hi There! It's "sunny" out" by using the + sign.
+
+> "Hi There! " + "It\'s \"sunny\" out";
+// expected output: "Hi There! It's "sunny" out"
