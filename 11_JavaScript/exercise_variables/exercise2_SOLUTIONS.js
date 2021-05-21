ec758e34f277ca0fdce75d0b5e84fdb7effb3b9f
smanwarulislam <engg.anwarbd@gmail.com>
2021-05-21 23:35:11 +0600

    Add exercise: variables

---
+++ 11_JavaScript/exercise_variables/exercise2_SOLUTIONS.js
@@ -0,0 +1,37 @@
+// Evaluate what answers you would get if you run this in the Javascript Console in Google Chrome.
+
+// Answer the questions then check them by copying them and running them in the console yourself line by line.
+
+// Chrome DevTools Console (See the Chrome DevTools Console)
+
+// add variable "firstName" and "lastName" so that they equal your name.
+var firstName = "S M Anwarul";
+var lastName = "Islam";
+
+// create a variable that holds the answer of "firstName" + " " + "lastName".
+var fullName = firstName + " " + lastName;
+
+fullName;
+// expected output: "S M Anwarul Islam"
+
+// Evaluate this question.
+
+What is a + b?
+
+var a = 34;
+var b = 21;
+
+a + b;
+// expected output: 55
+
+a = 2;
+
+a + b;
+
+what is the answer here?
+// expected output: 23
+
+What is c equal to?
+
+var c;
+// expected output: undefined
