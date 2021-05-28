b80ef0efcd7eb6d8e7804c805be86beb7b8f1884
smanwarulislam <engg.anwarbd@gmail.com>
2021-05-28 20:10:10 +0600

    Add javascript on our webpage

---
+++ 11_JavaScript/javascript_on_our_webpage/script.js
@@ -0,0 +1,25 @@
+4 + 3;
+
+if (4 + 3 === 7) {
+  // alert("You're smart!");
+  console.log("Hellooooo");
+}
+
+// Chrome DevTools Console (See the Chrome DevTools Console)
+
+// expected output: Hellooooo
+
+console.log("Hellooooo");
+// expected output: Hellooooo
+
+console.log("Hellooooo");
+// expected output: Hellooooo
+
+console.log("Hellooooo");
+// expected output: Hellooooo
+
+console.log("Hellooooo");
+// expected output: Hellooooo
+
+console.log("Hellooooo");
+// expected output: Hellooooo
