e04dae352a9ff74d0b03348cfaa7788674c38000
smanwarulislam <engg.anwarbd@gmail.com>
2022-02-02 22:45:13 +0600

    Add javascript keywords

---
+++ 11_JavaScript/javascript_keywords/javascript_keywords.js
@@ -0,0 +1,55 @@
+// JavaScript Keywords
+
+JAVASCRIPT KEYWORDS
+-------------------
+break
+case
+catch
+class
+const
+continue
+debugger
+default
+delete
+do
+else
+export
+extends
+finally
+for
+function
+if
+import
+in
+instanceof
+new
+return
+super
+switch
+this
+throw
+try
+typeof
+var
+void
+while
+with
+yield
+
+// For example
+
+// Chrome DevTools Console (See the Chrome DevTools Console)
+
+var for = 7;
+
+// expected output:
+// Uncaught SyntaxError: Unexpected token 'for'
+
+// I get a Syntax Error. Because 'for' is used for the 'for' loop.
+
+var var = 8;
+
+// expected output:
+// Uncaught SyntaxError: Unexpected token 'var'
+
+// I get a Syntax Error. Because 'var' already means something in JavaScript.
